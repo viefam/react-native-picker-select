d2f779e73477792e3a96b4989550c8aa20c71879
quocvietdang97@gmail.com <quocvietdang97@gmail.comn>
2021-05-24 21:41:25 +0700

    bump 8.0.5

--- src/styles.js
+++ src/styles.js
@@ -10,6 +10,12 @@ export const defaultStyles = StyleSheet.create({
     },
     modalViewTop: {
         flex: 1,
+        backgroundColor:'rgba(0,0,0,0.6)',
+        position:'absolute',
+        top:0,
+        left:0,
+        right:0,
+        bottom:0
     },
     modalViewMiddle: {
         height: 45,
